76e53ef88cb4294e9aa599d84329f5fbbb303c66
Jonayed Ahmed Riduan <51864353+JonayedAhmed@users.noreply.github.com>
2025-12-23 21:08:23 -0500

    hidden the testimonial section

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
         <Education />
         <Projects />
         <Technologies />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Contact />
       </main>
       <Footer />
